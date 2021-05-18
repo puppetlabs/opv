3a2d0418f01f69535f166468b9f59e4cc37d8634
sheena <sheena@puppet.com>
2021-05-18 11:37:58 +0100

    (#1)add required attributes to check_http type

--- lib/puppet/type/check_http.rb
+++ lib/puppet/type/check_http.rb
@@ -11,7 +11,7 @@ check_http { 'https://www.example.com': }
 
 Use this to check whether a web server is responding correctly. This can be used both as a prerequisite (don't manage something if a dependency is unhealthy) or to check whether everything went right after managing something.
 EOS
-  features: [],
+  features: ['custom_insync'],
   attributes: {
     ensure: {
       type: 'Enum[present, absent]',
@@ -22,6 +22,60 @@ EOS
       type: 'String',
       desc: 'The URL to test.',
       behaviour: :namevar,
+    },
+    headers: {
+      type: 'Hash[String, String]',
+      desc: 'A hash of headers to pass along with the request.',
+      behaviour: :parameter,
+      default: {},
+    },
+    expected_statuses: {
+      type: 'Array[Integer]',
+      desc: 'An array of acceptable HTTP status codes. If a request returns one of these status codes, it is considered a success',
+      behaviour: :parameter,
+      default: 200,
+    },
+    body_matcher: {
+      type: 'Regexp',
+      desc: 'A request is considered a success if the body of the HTTP response matches this regular expression',
+      behaviour: :parameter,
+      default: //,
+    },
+    request_timeout: {
+      type: 'Numeric',
+      desc: 'Number of seconds for a single request to wait for a response to return a success before aborting.',
+      behaviour: :parameter,
+      default: 10,
+    },
+    retries: {
+      type: 'Integer',
+      desc: 'Number of requests to make before giving up.',
+      behaviour: :parameter,
+      default: 3,
+    },
+    backoff: {
+      type: 'Numeric',
+      desc: 'Initial number of seconds to wait between requests.',
+      behaviour: :parameter,
+      default: 1,
+    },
+    exponential_backoff_base: {
+      type: 'Numeric',
+      desc: 'Exponential base for the exponential backoff calculations.',
+      behaviour: :parameter,
+      default: 2,
+    },
+    max_backoff: {
+      type: 'Numeric',
+      desc: 'An upper limit to the backoff duration,.',
+      behaviour: :parameter,
+      default: 10,
+    },
+    timeout: {
+      type: 'Numeric',
+      desc: 'Number of seconds allocated overall for the check to return a success before giving up.',
+      behaviour: :parameter,
+      default: 60,
     },
   },
 )
