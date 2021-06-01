18c03ba082eff24942fa96c45f8ebe7a82e83868
sheena <sheena@puppet.com>
2021-06-01 13:12:18 +0100

    (maint) warning messages

--- lib/puppet/provider/check_http/check_http.rb
+++ lib/puppet/provider/check_http/check_http.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'net/http'
-require 'pry'
 require 'retriable'
 
 # Implementation for the check_http type using the Resource API.
@@ -13,32 +12,42 @@ class Puppet::Provider::CheckHttp::CheckHttp
   def set(context, changes)
   end
 
-  # Update the check_http provider to use the above attributes to execute up to retries number of times 
-  # with success being defined as having one of the expected_statuses 
+  # Update the check_http provider to use the above attributes to execute up to retries number of times
+  # with success being defined as having one of the expected_statuses
   # and the body of the response matches body_matcher while taking into account request_timeout.
 
   def insync?(context, name, attribute_name, is_hash, should_hash)
     context.debug("Checking whether #{attribute_name} is up-to-date")
     should_hash.each do |name, change|
-      #binding.pry
       uri = URI(change)
+
+      # Update the check_http provider to wait for backoff ** (exponential_backoff_base * (retries - 1) seconds between attempts (up to max_backoff)
+      base_interval = should_hash[:backoff] ** (should_hash[:exponential_backoff_base] * should_hash[:retries] - 1)
+
+      # This callback provides the exception that was raised in the current try, the try_number, the elapsed_time for all tries so far, and the time in seconds of the next_interval.
+      do_this_on_each_retry = Proc.new do |exception, try, elapsed_time, next_interval|
+        context.info("#{exception.class}: '#{exception.message}' - #{try} tries in #{elapsed_time} seconds and #{next_interval} seconds until the next try.")
+      end
+
       context.processing(uri.to_s, {}, {}, message: 'checking http') do
-        Retriable.configure do |c|
-          #binding.pry
-          # Execute up to retries number of times
-          c.tries = should_hash[:retries]
-
-          c.max_elapsed_time = should_hash[:request_timeout]
-          # Update the check_http provider to wait for backoff ** (exponential_backoff_base * (retries - 1) seconds between attempts (up to max_backoff)
-          c.base_interval = should_hash[:backoff] ** (should_hash[:exponential_backoff_base] * should_hash[:retries] - 1)
-          c.max_interval = should_hash[:max_backoff]
-
-          binding.pry
+        Retriable.retriable(on: Timeout::Error, tries: should_hash[:retries], max_elapsed_time: should_hash[:request_timeout], base_interval: base_interval, max_interval: should_hash[:max_backoff], on_retry: do_this_on_each_retry) do
           response = Net::HTTP.get_response(uri)
+
           # Success being defined as having one of the expected_statuses and the body of the response matches body_matcher
-          if (should_hash[:expected_statuses].include? response.code) && (response.message.match(should_hash[:body_matcher]))
+          if (should_hash[:expected_statuses].include? response.code.to_i)
+            context.info("The return  response #{response.code} is matching with the expected_statuses #{should_hash[:expected_statuses]}")
+            if (response.body.match(should_hash[:body_matcher]))
+              context.info("The return response body #{response.body} is matching with body_matcher #{should_hash[:body_matcher]}")
+            else
+              raise Puppet::Error, "check_http response body check failed. The return response body #{response.body} is not matching body_matcher #{should_hash[:body_matcher]}"
+            end
             context.info("successfully connected to #{name}")
+          else
+            raise Puppet::Error, "check_http response code check failed. The return response #{response.code} is not matching with the expected_statuses #{should_hash[:expected_statuses]}"
           end
+        rescue Exception => ex
+          Puppet.alert("Exception during check_http. ex is #{ex} and backtrace is #{ex.backtrace}")
+          raise
         end
       end
     end
