5aa122e2a4c639152f448e8aa42c71302a6fbbf7
sheena <sheena@puppet.com>
2021-07-06 10:17:13 +0100

    (maint) remove invalid tests

--- spec/unit/puppet/provider/check_tcp_port/check_tcp_port_spec.rb
+++ spec/unit/puppet/provider/check_tcp_port/check_tcp_port_spec.rb
@@ -10,43 +10,9 @@ RSpec.describe Puppet::Provider::CheckTcpPort::CheckTcpPort do
 
   let(:context) { instance_double('Puppet::ResourceApi::BaseContext', 'context') }
 
-  describe '#get' do
+  describe 'get(context)' do
     it 'processes resources' do
-      expect(context).to receive(:debug).with('Returning pre-canned example data')
-      expect(provider.get(context)).to eq [
-        {
-          name: 'foo',
-          ensure: 'present',
-        },
-        {
-          name: 'bar',
-          ensure: 'present',
-        },
-      ]
-    end
-  end
-
-  describe 'create(context, name, should)' do
-    it 'creates the resource' do
-      expect(context).to receive(:notice).with(%r{\ACreating 'a'})
-
-      provider.create(context, 'a', name: 'a', ensure: 'present')
-    end
-  end
-
-  describe 'update(context, name, should)' do
-    it 'updates the resource' do
-      expect(context).to receive(:notice).with(%r{\AUpdating 'foo'})
-
-      provider.update(context, 'foo', name: 'foo', ensure: 'present')
-    end
-  end
-
-  describe 'delete(context, name)' do
-    it 'deletes the resource' do
-      expect(context).to receive(:notice).with(%r{\ADeleting 'foo'})
-
-      provider.delete(context, 'foo')
+      expect(provider.get(context)).to eq []
     end
   end
 end
